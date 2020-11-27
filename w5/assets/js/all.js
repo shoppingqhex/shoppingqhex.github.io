6eb32a7a3eaf3ee43f64e327c488b75ae59fffcf
shoppingq <shoppingqgjun@gmail.com>
2020-11-28 02:25:08 +0800

    最初完成版上傳

--- w5/assets/js/all.js
+++ w5/assets/js/all.js
@@ -1,7 +1,17 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HexSchool Hello!');
-});
+function currentPage() {
+  var currentUrl = window.location.pathname.split('/')[2];
+  console.log(currentUrl);
+  var assignment = document.querySelector('.assignment');
+  var admin = document.querySelector('.admin');
+
+  if (currentUrl === 'index.html') {
+    assignment.classList.add('active');
+  } else if (currentUrl === 'admin.html') {
+    admin.classList.add('active');
+  }
+}
+
+currentPage();
 //# sourceMappingURL=all.js.map
