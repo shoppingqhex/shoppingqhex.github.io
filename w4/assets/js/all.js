467642a17f578b3d2b6186a328006a76ccd4c00c
shoppingq <shoppingqgjun@gmail.com>
2020-11-28 16:04:24 +0800

    第二次訂正
    1.<h2>標籤
    2.手機版本container增加max-width
    3.scss巢狀調整
    4.is位置調整

--- w4/assets/js/all.js
+++ w4/assets/js/all.js
@@ -1,7 +1,31 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+/////// series.html 分頁 ///////
+var btns = document.querySelectorAll('.btn');
+var contents = document.querySelectorAll('.content');
+
+var _loop = function _loop(i) {
+  var index = i;
+  var btn = btns[i];
+  var content = contents[i];
+
+  btns[i].onclick = function () {
+    // console.log(index);
+    remove_active();
+    btn.classList.add('active');
+    content.classList.add('active');
+  };
+};
+
+for (var i = 0; i < btns.length; i++) {
+  _loop(i);
+}
+
+function remove_active() {
+  for (var _i = 0; _i < btns.length; _i++) {
+    btns[_i].classList.remove('active');
+
+    contents[_i].classList.remove('active');
+  }
+} /////// 分頁 ///////
 //# sourceMappingURL=all.js.map
